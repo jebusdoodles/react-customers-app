00da023ca4f34b48770474d12cc0539d79a006f4
ilhuitem0c <jc11111001001@gmail.com>
2020-02-15 18:09:09 -0600

    Utilizacion del route exact

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React from 'react';
-import { Link, BrowserRouter as Router } from 'react-router-dom';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Link to='/customers'>Customers</Link> <br></br>
-        <Link to='/customers/30000000'>Customer 30 000 000</Link>
-      </div>
-    </Router>
-  );
+class App extends Component{
+  
+  renderHome = () => <h1>Home</h1>;
+  
+  renderCustomerContainer = () => <h1>Customer Container</h1>; 
+  
+  renderCustomerListContainer = () => <h1>Customer list container</h1>;
+
+  renderCustomerNewContainer = () => <h1>Customer new container</h1>;
+
+  render(){
+    return(
+      <Router>
+        <div>
+          <Route exact path="/" component={this.renderHome}></Route>
+          <Route exact path="/customers" component={this.renderCustomerListContainer}></Route>
+          <Route exact path="/customer/:dni" component={this.renderCustomerContainer}></Route>
+          <Route exact path="/customers/new" component={this.renderCustomerNewContainer}></Route>
+        </div>
+      </Router>
+    )
+  }
 }
 
 export default App;
