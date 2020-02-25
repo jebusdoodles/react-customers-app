61f1e823622cd157bd213b492b0b28d1262c72d3
ilhuitem0c <jc11111001001@gmail.com>
2020-02-24 22:03:57 -0600

    se agrega redux actions

--- src/actions/fetchCustomers.js
+++ src/actions/fetchCustomers.js
@@ -1,5 +1,4 @@
+import { FETCH_CUSTOMERS } from "./../constants"; 
+import { createAction } from "redux-actions"
 
-export const fetchCustomers = () => {
-    debugger; 
-    return { type: 'FETCH_CUSTOMERS', payload: null }
-};
+export const fetchCustomers = createAction(FETCH_CUSTOMERS); 
