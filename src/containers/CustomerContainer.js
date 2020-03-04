0e1b47ceb674079e1202bb9ffda14be30ce9d783
ilhuitem0c <jc11111001001@gmail.com>
2020-03-04 14:19:48 -0600

    reselector y almacenamiento de informacion del cliente

--- src/containers/CustomerContainer.js
+++ src/containers/CustomerContainer.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'; 
 import PropTypes from 'prop-types';
 import AppFrame from './../components/AppFrame'
+import { getCustomersByDni } from '../selectors/customers';
 
 class CustomerContainer extends Component {
     render() {
         return (
             <div>
                 <AppFrame header={`Cliente ${this.props.dni}`} 
-                          body={<p>Datos del cliente</p>} >
+                          body={<p>Datos del cliente {this.props.customer.name} </p>} >
                 </AppFrame>
             </div>
         );
@@ -17,6 +18,11 @@ class CustomerContainer extends Component {
 
 CustomerContainer.propTypes = {
     dni: PropTypes.string.isRequired,
+    customer: PropTypes.object.isRequired,
 };
 
-export default connect(null, null)(CustomerContainer);
+const mapStateToProps = (state, props) => ({
+    customer: getCustomersByDni(state, props),
+});
+
+export default connect(mapStateToProps, null)(CustomerContainer);
