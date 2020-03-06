b1300d79c5cfee8596b06b94b142c5db236e4d50
ilhuitem0c <jc11111001001@gmail.com>
2020-03-06 15:11:26 -0600

    Ruteo dinamico

--- src/containers/CustomerContainer.js
+++ src/containers/CustomerContainer.js
@@ -3,13 +3,20 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import AppFrame from './../components/AppFrame'
 import { getCustomersByDni } from '../selectors/customers';
+import { Route } from 'react-router-dom';
 
 class CustomerContainer extends Component {
+    renderBody = () =>(
+        <Route path="/customer/:dni/edit" children={
+            ({ match }) => (match ? <p>Es Edicion</p> : <p>No es edicion</p>)
+        }></Route>
+    )
     render() {
         return (
+            //<p>Datos del cliente {this.props.customer.name} </p>
             <div>
                 <AppFrame header={`Cliente ${this.props.dni}`} 
-                          body={<p>Datos del cliente {this.props.customer.name} </p>} >
+                          body={this.renderBody()} >
                 </AppFrame>
             </div>
         );
