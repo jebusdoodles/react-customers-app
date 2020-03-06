908a7a627c0912cf749056c71527335e4d87c84a
ilhuitem0c <jc11111001001@gmail.com>
2020-03-06 15:26:28 -0600

    componentes edit y data

--- src/containers/CustomerContainer.js
+++ src/containers/CustomerContainer.js
@@ -4,11 +4,19 @@ import PropTypes from 'prop-types';
 import AppFrame from './../components/AppFrame'
 import { getCustomersByDni } from '../selectors/customers';
 import { Route } from 'react-router-dom';
+import CustomerEdit from './../components/CustomerEdit';
+import CustomerData from './../components/CustomerData';
 
 class CustomerContainer extends Component {
     renderBody = () =>(
         <Route path="/customer/:dni/edit" children={
-            ({ match }) => (match ? <p>Es Edicion</p> : <p>No es edicion</p>)
+            //nueva version de Ruteo dinamico de React Router
+            ({ match }) => (
+                match ? 
+                    // destructuring para evitar mencionar cada uno de los elementos
+                    <CustomerEdit {...this.props.customer}  />
+                : 
+                    <CustomerData {...this.props.customer} />)
         }></Route>
     )
     render() {
